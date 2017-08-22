20c7e278dcdd89030c1e1048fe824ec729a18a0f
ll <852191889@qq.com>
2017-08-22 23:50:07 +0800

    详情页 特效完成

--- assets/js/index.js
+++ assets/js/index.js
@@ -113,7 +113,7 @@ $(function() {
             var strHtml = "";
             $.each(data, function(key, value) {
                 strHtml += `<li class="showsmall">
-                                <a href="">
+                                <a href="./show.html">
                                     <div>
                                         <img src="${value.img}">
                                         <img src="assets/image/watch/result-picto_poincon.png">
@@ -130,6 +130,85 @@ $(function() {
     }
 
     // ==============================     list     end       =============================
+
+
+    // ==============================     show     start       =============================
+
+    // 小图移动事件
+    $('.wshowsmall a').hover(function() {
+        $('.wshowsmall a').css('opacity', '1');
+        $('.wshowsmall a').not($(this)).css('opacity', '0.8');
+    })
+
+    // 点击小图 切换大图
+    $('.wshowsmall a').click(function() {
+        var m = $(this).index();
+        $('.wshow >div').css('margin-left', '-150px');
+        $('.wshowbig a').hide();
+        $('.wshowsmall a').hide();
+        $('.shop').hide();
+        $('.wshowbig a').eq(m).fadeIn(function() {
+            $('.wshowbig').animate({
+                'margin-left': '0px'
+            }, 800, function() {
+                $('.shop').fadeIn(function() {
+                    $('.wshowsmall a').not($('.wshowsmall a').eq(m)).fadeIn(function() {
+                        $('.wshowsmall a').css('opacity', '1');
+                        $('.wshowsmall').animate({
+                            'margin-left': '125px'
+                        }, 400)
+                    });
+                })
+            })
+        })
+    });
+
+    // 点击侧边栏图片 显示img 大图
+
+    $('.wshowbig').click(function() {
+        $('.container').hide();
+        $('.another').show();
+        top()
+    })
+
+    $('.wssmall').click(function() {
+        $('.container').hide();
+        $('.another').show();
+        bottom();
+    })
+
+    $('.atype').click(function() {
+        top();
+    })
+
+    $('.adetial').click(function() {
+        bottom();
+    })
+
+    $('.close').click(function() {
+        $('.another').hide();
+        $('.container').show();
+    })
+
+    // 移动鼠标  改变img位置
+
+    $('.another').mousemove(function(e) {
+        console.log(e.pageY);
+        var X = (e.pageX / 782) * 320;
+        var Y = (e.pageY / 130) * 260;
+        $('.another>img').css({ 'margin-left': -X + 'px', 'margin-top': -Y + 'px' });
+    })
+
+    function top() {
+        $('.another>img').attr('src', './assets/image/watch/image.scale.2000.2000.1489408770449.jpeg');
+    }
+
+    function bottom() {
+        $('.another>img').attr('src', './assets/image/watch/image.scale.2000.2000.1489408770479.jpeg');
+    }
+
+
+    // ==============================     show     end       =============================
 
 
     // ==============================     login     start    =============================
